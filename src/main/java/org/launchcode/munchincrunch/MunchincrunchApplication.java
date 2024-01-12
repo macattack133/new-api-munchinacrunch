1ffddf2bc7044d4018989b2e3fd1ba56f8221fc9
Sabriya Sowers <sabriyasowers@gmail.com>
2024-01-11 19:50:09 -0600

    Intial build of API

---
+++ src/main/java/org/launchcode/munchincrunch/MunchincrunchApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.munchincrunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchincrunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunchincrunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/munchincrunch/MunchincrunchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.munchincrunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunchincrunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
