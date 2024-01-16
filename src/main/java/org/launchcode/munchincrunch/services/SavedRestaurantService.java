3fcb5b5a39d27cf5e3c3242f8ddf06b6d7e4fb17
Sabriya Sowers <sabriyasowers@gmail.com>
2024-01-15 18:19:10 -0600

    Testing code

--- src/main/java/org/launchcode/munchincrunch/services/SavedRestaurantService.java
+++ src/main/java/org/launchcode/munchincrunch/services/SavedRestaurantService.java
@@ -4,7 +4,6 @@ import org.launchcode.munchincrunch.models.SavedRestaurantEntity;
 import org.launchcode.munchincrunch.models.data.SavedRestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
