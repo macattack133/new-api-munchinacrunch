f0625ed34458e4418a46a1707e309b30f59c6648
noah.brandwein1 <noah.brandwein98@gmail.com>
2024-01-17 19:07:47 -0600

    Creating CRUD functionality in API pt.2

--- src/main/java/org/launchcode/munchincrunch/services/RestaurantService.java
+++ src/main/java/org/launchcode/munchincrunch/services/RestaurantService.java
@@ -5,16 +5,37 @@ import org.launchcode.munchincrunch.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-// RestaurantService.java
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RestaurantService {
 
     @Autowired
     private RestaurantRepository restaurantRepository;
 
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
     // Other methods...
 
     public Restaurant createRestaurant(Restaurant restaurant) {
         return restaurantRepository.save(restaurant);
     }
+
+    public Restaurant updateRestaurant(Long id, Restaurant updatedRestaurant) {
+        Optional<Restaurant> existingRestaurant = restaurantRepository.findById(id);
+        if (((Optional<?>) existingRestaurant).isPresent()) {
+            Restaurant restaurant = existingRestaurant.get();
+            restaurant.setName(updatedRestaurant.getName());
+            restaurant.setCuisine(updatedRestaurant.getCuisine());
+            return restaurantRepository.save(restaurant);
+        }
+        return null; // Handle not found case
+    }
+    public void deleteRestaurant(Long id) {
+        restaurantRepository.deleteById(id);
+    }
 }
+
